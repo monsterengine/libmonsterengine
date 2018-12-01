ee26f98c9905e046c462807938cbec81a0293f4b
Shogo Otake <shogo.otake@gmail.com>
2018-12-01 16:14:35 +0900

    Add functions for MonsterEngineConfig

--- src/monster_engine_config.rs
+++ src/monster_engine_config.rs
@@ -1,8 +1,29 @@
 use std::ffi::CString;
-use std::os::raw::c_uint;
+use std::os::raw::{c_char, c_uint};
 
 #[repr(C)]
 pub struct MonsterEngineConfig {
-    pub bind: *const CString,
+    pub bind: CString,
     pub workers: c_uint,
+}
+
+#[no_mangle]
+pub extern fn monster_engine_config_new(bind: *const c_char, workers: c_uint) -> *mut MonsterEngineConfig {
+    unsafe {
+        Box::into_raw(
+            Box::new(
+                MonsterEngineConfig {
+                    bind: CString::from_raw(bind as *mut c_char),
+                    workers: workers,
+                }
+            )
+        )
+    }
+}
+
+#[no_mangle]
+pub extern fn monster_engine_config_destroy(monster_engine_config: *mut MonsterEngineConfig) {
+    unsafe {
+       drop(Box::from_raw(monster_engine_config));
+    }
 }
